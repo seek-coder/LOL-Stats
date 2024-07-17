350b3deaf8b78d2b0d107831d0765e96bf96e32e
unknown <catrielgatto99@gmail.com>
2024-07-17 01:59:42 -0300

    fix: modify some functions

--- api_request.py
+++ api_request.py
@@ -27,7 +27,7 @@ class StatsApp:
     # -------------------- #
     def get_matches_list(self, region: str, player_puuid: str) -> str:
 
-        self.api_url= f"https://{region}.api.riotgames.com/lol/match/v5/matches/by-puuid/{player_puuid}/ids?start=0&count=20&api_key={self.api_key}" #LA2
+        self.api_url= f"https://{region}.api.riotgames.com/lol/match/v5/matches/by-puuid/{player_puuid}/ids?start=0&count=20&api_key={self.api_key}"
 
         self.matches_list = self.get_response(self.api_url)
         self.match_id = self.matches_list[0]  
@@ -37,21 +37,30 @@ class StatsApp:
     # ------------------ #
     #     3. match       #
     # ------------------ #
-    def get_match_data(self, region: str, match_id: int, player_puuid: str) -> str:
+    def get_match_data(self, region: str, match_id: int, player_puuid: str) -> dict:
 
         # ----- estructura de la api ----- #
         self.api_url = f"https://{region}.api.riotgames.com/lol/match/v5/matches/{match_id}?api_key={self.api_key}"
 
         self.match_data = self.get_response(self.api_url)
-        self.match_data.keys() # me devuelve dos llaves: metadata, info
-        self.match_data_info = self.match_data["info"]
-        #match_data_info_participants = match_data["info"]["participants"][0] # si solo quisiera la info del primer participante
-        #match_data_info_participants_champion_name = match_data["info"]["participants"][0]["championName"] # si solo quisiera el nombre del champ del primer participante
-        self.match_self_player_index = self.match_data["metadata"]["participants"].index(player_puuid)
-        self.match_data_self_player_info_champion = self.match_data["info"]["participants"][self.match_self_player_index]["championName"]
 
-        return self.match_data_self_player_info_champion
-    
+        self.player_index = self.match_data["metadata"]["participants"].index(player_puuid)
+        self.player_info = self.match_data["info"]["participants"][self.player_index]
+        
+        # KDA: (asesinatos + asistencias)/ muertes
+
+        return {
+            "win": self.player_info["win"],
+            "championName": self.player_info["championName"],
+            "teamId": self.player_info["teamId"],
+            "gameDuration": self.match_data["info"]["gameDuration"],
+            "gameCreation": self.match_data["info"]["gameCreation"],
+            "kills": self.player_info["kills"],
+            "deaths": self.player_info["deaths"],
+            "assists": self.player_info["assists"],
+            "goldEarned": self.player_info["goldEarned"],
+            "totalMinionsKilled": self.player_info["totalMinionsKilled"]
+        }
 
 # --- testeos --- #
 testing = StatsApp()
@@ -63,5 +72,16 @@ puuid = testing.get_player_puuid(region, game_name, tag_line)
 match_id = testing.get_matches_list(region, puuid)
 
 
-print(testing.get_match_data(region, match_id, puuid))
+#print(testing.get_match_data(region, match_id, puuid)) # me devuelve la info que yo necesite
+
+print(f"¿Ganó?: {testing.get_match_data(region, match_id, puuid)["win"]}")
+print(f"Nombre del champ: {testing.get_match_data(region, match_id, puuid)["championName"]}")
+print(f"Lado del mapa (100 azul, 200 rojo): {testing.get_match_data(region, match_id, puuid)["teamId"]}")
+print(f"Duración de la partida: {testing.get_match_data(region, match_id, puuid)["gameDuration"]}")
+print(f"Fecha de juego: {testing.get_match_data(region, match_id, puuid)["gameCreation"]}")
+print(f"Kills: {testing.get_match_data(region, match_id, puuid)["kills"]}")
+print(f"Muertes: {testing.get_match_data(region, match_id, puuid)["deaths"]}")
+print(f"Asistencias: {testing.get_match_data(region, match_id, puuid)["assists"]}")
+print(f"Oro ganado total: {testing.get_match_data(region, match_id, puuid)["goldEarned"]}")
+print(f"Minions eliminados total: {testing.get_match_data(region, match_id, puuid)["totalMinionsKilled"]}")
 # -------------- #s
