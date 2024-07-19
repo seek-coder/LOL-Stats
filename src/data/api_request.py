23cc9ce93adcc82f2869841aeb157f86b069d1eb
unknown <catrielgatto99@gmail.com>
2024-07-19 02:57:19 -0300

    fix: add new api key

---
+++ src/data/api_request.py
@@ -0,0 +1,98 @@
+import requests
+
+class StatsApp:
+    # ------------------ #
+    #    1. summoner     #
+    # ------------------ #
+    def __init__(self):
+        self.api_key = "RGAPI-9ee88349-1701-4813-9c91-31a46733d3ed"
+
+    def get_response(self, url: str) -> str:
+        self.response = requests.get(url)
+        #self.response.raise_for_status() # valida que esté todo bien en la salida
+        return self.response.json()
+    
+    def get_player_puuid(self, region: str, game_name: str, tag_line: str) -> str:
+        
+        self.api_url= f"https://{region}.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{game_name}/{tag_line}?api_key={self.api_key}"
+
+        self.player_data = self.get_response(self.api_url)
+
+        self.player_puuid = self.player_data["puuid"]
+
+        return self.player_puuid
+
+    # -------------------- #
+    # 2. lista de matches  #
+    # -------------------- #
+    def get_matches_list(self, region: str, player_puuid: str) -> list:
+
+        self.api_url= f"https://{region}.api.riotgames.com/lol/match/v5/matches/by-puuid/{player_puuid}/ids?start=0&count=20&api_key={self.api_key}"
+
+        self.matches_list = self.get_response(self.api_url)
+
+        return self.matches_list
+
+    # ------------------ #
+    #     3. match       #
+    # ------------------ #
+    def get_match_data(self, region: str, match_id: int, player_puuid: str) -> dict:
+
+        # ----- estructura de la api ----- #
+        self.api_url = f"https://{region}.api.riotgames.com/lol/match/v5/matches/{match_id}?api_key={self.api_key}"
+
+        self.match_data = self.get_response(self.api_url)
+
+        self.player_index = self.match_data["metadata"]["participants"].index(player_puuid)
+        self.player_info = self.match_data["info"]["participants"][self.player_index]
+        
+        # KDA: (asesinatos + asistencias)/ muertes
+
+        return {
+            "win": self.player_info["win"],
+            "championName": self.player_info["championName"],
+            "teamId": self.player_info["teamId"],
+            "gameDuration": self.match_data["info"]["gameDuration"], #ms
+            "gameCreation": self.match_data["info"]["gameCreation"], #unix
+            "kills": self.player_info["kills"],
+            "deaths": self.player_info["deaths"],
+            "assists": self.player_info["assists"],
+            "goldEarned": self.player_info["goldEarned"],
+            "totalMinionsKilled": self.player_info["totalMinionsKilled"]
+        }
+    
+    def get_every_match_data(self, region:str, matches_list: list, player_puuid: str) -> dict:
+        matches_dict = { }
+
+        for match_id in matches_list:
+            matches_dict[match_id] = self.get_match_data(region, match_id, player_puuid)
+        
+        return matches_dict
+    
+    def get_summoner_data(self, region_specify_n: str, player_puuid: str):
+        
+        self.api_url = f"https://{region_specify_n}.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/{player_puuid}?api_key={self.api_key}"
+
+        self.summoner_data = self.get_response(self.api_url)
+        self.summoner_data_level = self.summoner_data["summonerLevel"] # long
+        self.summoner_data_ico = self.summoner_data["profileIconId"] # int
+
+        return {
+            "summonerLevel": self.summoner_data_level,
+            "profileIconId": self.summoner_data_ico
+        }
+
+# --- testeos --- #
+testing = StatsApp()
+
+region = "americas"
+game_name  = input("Mandate el game name: ")
+tag_line = input("Mandate el tag line: ").upper()
+puuid = testing.get_player_puuid(region, game_name, tag_line)
+match_id = testing.get_matches_list(region, puuid)[0]
+
+#print(testing.get_every_match_data(region, testing.get_matches_list(region, puuid), puuid))
+print(testing.get_summoner_data("la2", puuid))
+
+#https://ddragon.leagueoflegends.com/cdn/14.14.1/img/profileicon/4560.png # iconos
+# -------------- #
---
+++ src/data/visualization_data.py
@@ -0,0 +1,74 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from process_data import MatchHistory
+from api_request import *
+
+class CreatePlt:
+    def __init__(self, df: pd.DataFrame):
+        self.df = df
+
+    def winrate_per_day_plot(self) -> plt.Figure:
+        match_history = MatchHistory(self.df)
+        match_history.get_match_day("gameCreation")
+        filtered_history = match_history.filter_by_win()
+        victories_by_day = filtered_history['gameCreation'].value_counts()
+
+        days_of_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+        victories_by_day = victories_by_day.reindex(days_of_week, fill_value=0)
+
+        total_victories = victories_by_day.sum()
+
+        if total_victories > 0:
+            win_rate_by_day = (victories_by_day / total_victories) * 100
+        else:
+            win_rate_by_day = pd.Series([0]*7, index=days_of_week)
+
+        fig, ax = plt.subplots()
+        win_rate_by_day.plot(kind='bar', ax=ax)
+        ax.set_xlabel('Día de la semana')
+        ax.set_ylabel('Porcentaje de victorias')
+        ax.set_title('Tasa de victorias según el día de la semana')
+        plt.show()
+
+        return fig
+
+    def winrate_per_hour_plot(self) -> plt.Figure:
+        match_history = MatchHistory(self.df)
+        match_history.get_match_hour("gameCreation")
+        filtered_history = match_history.filter_by_win()
+        victories_by_hour = filtered_history['gameCreation'].value_counts()
+        all_hours = pd.Series(0, index=range(24))
+        victories_by_hour = all_hours.add(victories_by_hour, fill_value=0)
+        total_victories = victories_by_hour.sum()
+        if total_victories > 0:
+            win_rate_by_hour = round((victories_by_hour / total_victories) * 100)
+        else:
+            win_rate_by_hour = 0 
+
+        fig, ax = plt.subplots()
+        win_rate_by_hour.plot(kind='bar', ax=ax)
+        ax.set_xlabel('Hora')
+        ax.set_ylabel('Porcentaje de victorias')
+        ax.set_title('Tasa de victorias según la hora del día')
+        ax.set_xticks([0, 5, 10, 15, 20])
+        ax.set_xticklabels([0, 5, 10, 15, 20])
+        plt.show()
+
+        return fig
+
+
+api_request_app = StatsApp()
+
+region = "americas"
+game_name  = input("Mandate el game name: ")
+tag_line = input("Mandate el tag line: ").upper()
+puuid = api_request_app.get_player_puuid(region, game_name, tag_line)
+matches_list = api_request_app.get_matches_list(region, puuid)
+dictionary = api_request_app.get_every_match_data(region, matches_list, puuid)
+
+df = pd.DataFrame.from_dict(dictionary, orient="index")
+match_history = MatchHistory(df)
+
+print(match_history.get_kda())
+# plot_creator = CreatePlt(df)
+# plot_creator.winrate_per_hour_plot()
